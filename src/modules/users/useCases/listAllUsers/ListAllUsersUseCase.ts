42b00ab493eb7f97175660315c0e5ebec7b4b5cf
jonatasmsouza <jonatasvu@hotmail.com>
2021-07-14 23:19:33 -0300

    comitando
    comitando

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,11 +6,28 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const users = this.usersRepository.list();
+    if (!user_id) {
+      return users;
+    }
+
+    const userAlreadyExists = this.usersRepository.findById(user_id);
+    if (!userAlreadyExists) {
+      throw new Error("User not exists!");
+    }
+    const adm = userAlreadyExists.admin;
+    if (!adm) {
+      throw new Error("User not admin!");
+
+    }
+
+    const user = users.filter((user) => user.id === user_id);
+    return user;
   }
 }
 
 export { ListAllUsersUseCase };
+
